770181cfbbfbcc155d37ce3f4d7aa162f7df6c5f
Vishwanath-Bhat <vishwasbhat2003@gmail.com>
2024-12-22 15:47:46 +0530

    first Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// var crsr = document.querySelector("#cursor")
+// var blur1 = document.querySelector("#cursor-blur")
+
+
+// document.addEventListener("mousemove",function(dets){
+//     crsr.style.left = dets.x + "px"
+//     crsr.style.top = dets.y + "px"
+//     blur1.style.left = dets.x - 250 + "px"
+//     blur1.style.top = dets.y - 250 + "px"
+// })
+
+
+// var crsr = document.querySelector("#cursor");
+// var blur1 = document.querySelector("#cursor-blur");
+
+// let mousePos = { x: 0, y: 0 };
+
+// document.addEventListener("mousemove", (dets) => {
+//     mousePos.x = dets.x;
+//     mousePos.y = dets.y;
+// });
+
+// function updateCursor() {
+//     crsr.style.left = mousePos.x + "px";
+//     crsr.style.top = mousePos.y + "px";
+//     blur1.style.left = mousePos.x - 250 + "px";
+//     blur1.style.top = mousePos.y - 250 + "px";
+//     requestAnimationFrame(updateCursor);
+// }
+// updateCursor();
